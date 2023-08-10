191fdcf98cafb44cc06e45165ccc71394853392b
shannon <x.liu0779@gmail.com>
2023-08-10 11:15:36 +0800

    feat: 🎸 增加 APP_CONFIG__ 前缀环境变量聚合为 APP_CONFIG

---
+++ README.md
@@ -0,0 +1,35 @@
+# Webrunner
+
+Simple webapp runner
+
+# Configuration
+
+## `APP_ROOT`
+
+```
+app/
+    __built__/
+        xxx.js
+        xxx.css
+    static/
+        xxx.js
+        xxx.css
+    sw.js
+    favicon.ico
+    index.html
+```
+
+## `ENV` 
+
+be used to replace `__ENV__` in `index.html`
+
+## `PROJECT_VERSION` 
+
+be used to replace `__PROJECT_VERSION__` in `index.html`
+
+## `APP_CONFIG` or `APP_CONFIG__*`
+
+be used to replace `__APP_CONFIG__` in `index.html`
+
+* format: `key1=value1,key2=value2`
+* `APP_CONFIG__*` will join to `APP_CONFIG`
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,28 @@
 
 # This script is used to start the application
 
+# 初始化一个字符串，用于存储拼接后的值
+app_config="${APP_CONFIG}"
+
+# 遍历所有环境变量
+for var in $(env | cut -d= -f1); do
+    # 检查变量是否以 "APP_CONFIG__" 开头
+    if [ "$(echo "$var" | grep '^APP_CONFIG__')" ]; then
+        # 去除变量名前缀 "APP_CONFIG__"
+        trimmed_var=$(echo "$var" | sed 's/^APP_CONFIG__//')
+        # 使用 eval 来获取变量值并拼接到字符串中
+        value=$(eval echo "\$$var")
+        app_config="${app_config},${trimmed_var}=${value}"
+    fi
+done
+
+# 去掉起始的逗号
+export app_config=$(echo "$app_config" | sed 's/^,//')
+
+
 # Install envsubst
 echo "Installing envsubst"
-envsubst '${PROJECT_VERSION} ${ENV} ${APP_CONFIG}' < /etc/nginx/conf.d/conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PROJECT_VERSION} ${ENV} ${app_config}' < /etc/nginx/conf.d/conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 echo "Starting nginx"
